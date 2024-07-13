b07852756071e48c9ef063b813024c0283b339fc
UdaykiranRegimudi <uuday3804@gmail.com>
2024-07-13 15:26:16 +0530

    empty_commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dotenv.config();
 const MONGO_URI = process.env.MONGODB_URI;
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch((error) => console.error('MongoDB connection error:', error));
 
